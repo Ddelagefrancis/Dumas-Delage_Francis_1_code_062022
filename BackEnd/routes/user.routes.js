27335b708f4414f4db58f74fc5a8f0a56f005faf
FrancisDumas-Delage_3_25012022 <dumasdelagefrancis@gmail.com>
2022-06-30 13:39:34 +0200

    Ajout routes middleware vérification pseudo

--- BackEnd/routes/user.routes.js
+++ BackEnd/routes/user.routes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 // importation des middleware 
 const registrer = require ("../middleware/signup.middleware");
+const pseudo = require('../middleware/username.middleware');
 const email = require ("../middleware/email.middleware");
 const password = require ("../middleware/password.middleware");
 const connexion = require('../middleware/limit.middleware');
@@ -10,7 +11,7 @@ const connexion = require('../middleware/limit.middleware');
 // importation controllers 
 const userCtrl = require("../controllers/user.controller");
 
-router.post("/signup", registrer, email, password, userCtrl.signup);
+router.post("/signup", registrer, pseudo, email, password, userCtrl.signup);
 router.post("/login", connexion, userCtrl.login);
 
 module.exports = router;
