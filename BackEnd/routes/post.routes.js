cd7e946a7915edfafdeaefa63785f24d6d0e8cc3
FrancisDumas-Delage_3_25012022 <dumasdelagefrancis@gmail.com>
2022-07-09 18:01:13 +0200

    Ajout des routes Like et Dislike

--- BackEnd/routes/post.routes.js
+++ BackEnd/routes/post.routes.js
@@ -7,11 +7,16 @@ const auth = require ("../middleware/auth.middleware");
 
 // importation controllers 
 const postCtrl = require("../controllers/post.controller");
+const likeCtrl = require('../controllers/like.controller');
 
-
+// Routes de l'API pour les messages
 router.post("/", auth, postCtrl.createPost);
 router.get("/", postCtrl.getAllPosts);
 router.put('/:id', auth, postCtrl.modifyPost);
 router.delete('/:id', auth, postCtrl.deletePost);
+
+// Routes de l'API pour les likes
+router.post('/:postId/like', auth, likeCtrl.likePost);
+router.post('/:postId/dislike', auth, likeCtrl.dislikePost);
 
 module.exports = router;
