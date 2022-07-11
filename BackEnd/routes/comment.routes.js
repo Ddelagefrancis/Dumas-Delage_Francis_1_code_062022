35e1dabf42cf12f9a1a0b7db1bb651dad25c18ba
FrancisDumas-Delage_3_25012022 <dumasdelagefrancis@gmail.com>
2022-07-11 16:22:07 +0200

    Ajout route "delete comment"

--- BackEnd/routes/comment.routes.js
+++ BackEnd/routes/comment.routes.js
@@ -13,7 +13,7 @@ const commentCtrl = require('../controllers/comment.controller');
 // Routes de l'API pour les messages
 router.post('/:postId', auth, commentCtrl.createComment);
 router.get('/:postId', auth, commentCtrl.getAllComments);
-// router.delete('/:commentId', auth, commentCtrl.deleteComment);
+router.delete('/:commentId', auth, commentCtrl.deleteComment);
 
 
 // Permet d'exporter le router
