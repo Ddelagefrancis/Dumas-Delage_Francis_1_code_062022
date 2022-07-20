28c4cc94e48cda5670e6e602de9f3b48a058de78
FrancisDumas-Delage_3_25012022 <dumasdelagefrancis@gmail.com>
2022-07-20 14:17:07 +0200

    ajout des routes multer et de l'admin

--- BackEnd/routes/post.routes.js
+++ BackEnd/routes/post.routes.js
@@ -3,17 +3,20 @@ const router = express.Router();
 
 // importation des middleware 
 const auth = require ("../middleware/auth.middleware");
-// const multer = require('../middleware/multer-config');
+const multer = require('../middleware/multer-config');
 
 // importation controllers 
 const postCtrl = require("../controllers/post.controller");
 const likeCtrl = require('../controllers/like.controller');
 
 // Routes de l'API pour les messages
-router.post("/", auth, postCtrl.createPost);
+router.post("/", auth, multer, postCtrl.createPost);
 router.get("/", postCtrl.getAllPosts);
-router.put('/:id', auth, postCtrl.modifyPost);
+router.put('/:id', auth, multer, postCtrl.modifyPost);
 router.delete('/:id', auth, postCtrl.deletePost);
+
+// Requête DELETE de l'administrateur pour supprimer un post  ***/
+router.delete('/admin/:id', auth, multer, postCtrl.adminDeletePost);
 
 // Routes de l'API pour les likes
 router.post('/:postId/like', auth, likeCtrl.likePost);
