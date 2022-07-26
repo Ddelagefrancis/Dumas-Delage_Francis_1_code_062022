d10cd67513939ec0e86356d5bd0384cd384933c8
FrancisDumas-Delage_3_25012022 <dumasdelagefrancis@gmail.com>
2022-07-26 13:42:52 +0200

    Implementation des routes des differentes pages

--- FrontEnd/groupomania/src/App.js
+++ FrontEnd/groupomania/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from 'react-router-dom'
+import Accueil from './components/accueil/Accueil'
+import Home from './components/home/Home'
+import Signup from './components/accueil/signup/Signup'
+import Login from './components/accueil/login/Login';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Routes>
+            <Route path="/" element={<Accueil />} >
+              <Route path="/signup" element={<Signup/>} />
+              <Route path="/login" element={<Login/>} />
+            </Route>
+            <Route path="/home" element={<Home />} />
+      </Routes>
     </div>
   );
 }
