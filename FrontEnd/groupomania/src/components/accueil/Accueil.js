b8fc6d2bc1775350c98bda93322253f617f7c213
FrancisDumas-Delage_3_25012022 <dumasdelagefrancis@gmail.com>
2022-07-28 19:33:31 +0200

    First commit page d'Accueil

--- FrontEnd/groupomania/src/components/accueil/Accueil.js
+++ FrontEnd/groupomania/src/components/accueil/Accueil.js
@@ -1,16 +1,22 @@
 import { Link, Outlet } from 'react-router-dom';
 
 function Accueil() {
-    return (
-      <div >
-        <h1>Groupomania</h1>
-        <nav>
+  return (
+    <div >
+      <div className="accueil-container">
+        <img src="./img/icon-left-font.png" alt="img-acceuil" />
+        <p>Réseau social interne pour les employés</p>
+      </div >
+      {/* <div className="accueil-social_img">
+        <img src="./img/social_network.png" alt="img-acceuil" />
+      </div > */}
+      <nav>
         <Link to="/signup">S'inscrire</Link>
         <Link to="/login">Se connecter</Link>
       </nav>
-      <Outlet/>
-      </div>
-    );
-  }
-  
-  export default Accueil;
+      <Outlet />
+    </div>
+  );
+}
+
+export default Accueil;
