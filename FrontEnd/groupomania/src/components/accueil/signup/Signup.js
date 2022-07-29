ffd1ade1523a4927afbafd524e4f9d6930553afb
FrancisDumas-Delage_3_25012022 <dumasdelagefrancis@gmail.com>
2022-07-29 11:07:15 +0200

    Ajout du formulaire d'inscription

--- FrontEnd/groupomania/src/components/accueil/signup/Signup.js
+++ FrontEnd/groupomania/src/components/accueil/signup/Signup.js
@@ -1,10 +1,65 @@
+import React, { useState } from 'react';
+import axios from 'axios';
 
 function Signup() {
+    const [pseudo, setPseudo] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [controlPassword, setControlPassword] = useState('');
+
+    const handleRegister = async (e) => {
+
+    }
+
     return (
-      <div >
-        <p>Ici formulaire d'Inscription</p>
-      </div>
+        <form action="" onSubmit={handleRegister} id="signup">
+            <label htmlFor="pseudo">Pseudo</label>
+            <br />
+            <input
+                type="text"
+                name="pseudo"
+                id="pseudo"
+                onChange={(e) => setPseudo(e.target.value)}
+                value={pseudo}
+            />
+            <div className="pseudo error"></div>
+            <br />
+            <label htmlFor="email">Email</label>
+            <br />
+            <input
+                type="text"
+                name="email"
+                id="email"
+                onChange={(e) => setEmail(e.target.value)}
+                value={email}
+            />
+            <div className="email error"></div>
+            <br />
+            <label htmlFor="password">Mot de passe</label>
+            <br />
+            <input
+                type="password"
+                name="password"
+                id="password-conf"
+                onChange={(e) => setPassword(e.target.value)}
+                value={password}
+            />
+            <div className="password error"></div>
+            <br />
+            <label htmlFor="password-conf">Confirmer mot de passe</label>
+            <br />
+            <input
+                type="password"
+                name="password"
+                id="password"
+                onChange={(e) => setControlPassword(e.target.value)}
+                value={controlPassword}
+            />
+            <div className="password-confirm error"></div>
+            <br />
+            <input type="submit" value="Valider inscription" />
+        </form>
     );
-  }
-  
-  export default Signup;
+}
+
+export default Signup;
